23cac4d8aa43f6f2ca8175803858649db96ed40b
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-09-14 10:08:40 +0500

    update post

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
-
+from django.utils.safestring import mark_safe
+from .models import *
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','title','icon')
+    list_display_links = ['title']
+    list_filter = ['id']
+    # list_editable = ['title']
+    
+    prepopulated_fields = {"slug":('title',)}
+    
+    def get_obj_icon(self, obj):
+        return mark_safe(f'<i class="{obj.icon}"><i/>')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id','title','icon')
+    list_display_links = ['title']
+    list_filter = ['id']
+    # list_editable = ['title']
+    
+    prepopulated_fields = {"slug":('title',)}
+    
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('get_obj_image','title','author','views')
+    list_display_links = ['get_obj_image','title']
+    list_filter = ['author','views', 'category']
+    list_per_page = 10
+    # list_editable = ['title']
+    
+    prepopulated_fields = {"slug":('title',)}
+    
+    def get_obj_image(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" width="50" />')
