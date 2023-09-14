151aa1868e122aa90b0650fb8de827d6886725d9
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-09-14 11:27:03 +0500

    update

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , HttpResponse
 from django.views.generic import ListView
+from faker import Faker
 
 from .models import Post
 
 # Create your views here.
 class PostListView(ListView):
     model = Post
+
+
+def generate_data(request):
+    fake = Faker()
+    # for i in range(10):
+        # Post.objects.create()
+    return HttpResponse("<span>done</span>")
