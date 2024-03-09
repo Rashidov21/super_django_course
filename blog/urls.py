f12290a8930b70f3a7806b5e2d145e5fa9d48f5c
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-03-09 10:56:30 +0500

    delete old projects

--- blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path("", views.PostListView.as_view(), name='blog'),
-    path('gen/', views.generate_data)
-]
--- player/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path("player/", views.PlayerListView.as_view(), name='player_list'),
-    path('add/', views.PlayerAdView.as_view(), name='add')
-]
