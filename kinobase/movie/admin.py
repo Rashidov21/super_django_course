3657a70cb2be15e4057dd3dead0c5da4a4666443
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-05-22 20:58:34 +0500

    update db

--- kinobase/movie/admin.py
+++ kinobase/movie/admin.py
@@ -2,9 +2,35 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Author)
-admin.site.register(Category)
+
+
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("id","name")
+    prepopulated_fields = {"slug":("name",)}
+    
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id","name")
+    prepopulated_fields = {"slug":("name",)}
+    
+    
+@admin.register(Genre)
+class GanreAdmin(admin.ModelAdmin):
+    list_display = ("id","name")
+    prepopulated_fields = {"slug":("name",)}
+    
+    
+    
 admin.site.register(Comment)
-admin.site.register(Genre)
-admin.site.register(Movie)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("id","title")
+    prepopulated_fields = {"slug":("title",)}
+    
 admin.site.register(Role)
